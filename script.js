0af64836d6084f4bdd19412d0a9d860bf00446a3
Akram Ansari <119692799+akram222001@users.noreply.github.com>
2024-03-24 14:50:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 function fibonacci(num) {
-// your code here
+    if (num <= 0) {
+        return "Invalid input";
+    } else if (num === 1) {
+        return 0;
+    } else if (num === 2) {
+        return 1;
+    }
+
+    let fib1 = 0;
+    let fib2 = 1;
+    for (let i = 2; i < num; i++) {
+        let fib = fib1 + fib2;
+        fib1 = fib2;
+        fib2 = fib;
+    }
+
+    return fib2;
 }
 
 module.exports = fibonacci;
